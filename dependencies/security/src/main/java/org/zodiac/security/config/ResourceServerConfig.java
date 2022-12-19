787d7111afbc858b105f00a88847a3b41499a2f9
Pavel Gutsu <pavelgutsu@coherentsolutions.com>
2022-12-19 19:23:13 +0100

    Add basic security

--- dependencies/security/src/main/java/org/zodiac/security/config/ResourceServerConfig.java
+++ dependencies/security/src/main/java/org/zodiac/security/config/ResourceServerConfig.java
@@ -3,19 +3,18 @@ package org.zodiac.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ResourceServerConfig {
-
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf().disable()
-				.authorizeHttpRequests()
-				.anyRequest().permitAll();
-
+				.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+				.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
 		return http.build();
 	}
 
--- user-service/src/main/java/org/zodiac/userservice/controller/UserController.java
+++ user-service/src/main/java/org/zodiac/userservice/controller/UserController.java
@@ -3,8 +3,6 @@ package org.zodiac.userservice.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.zodiac.userservice.converter.UserConverter;
 import org.zodiac.userservice.dto.UserDto;
