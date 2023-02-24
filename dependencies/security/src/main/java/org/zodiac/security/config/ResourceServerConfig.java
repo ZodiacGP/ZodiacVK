04a7b63bf56ff6f6557d4cbfd6b0fe34700e2e17
Pavel Gutsu <pavelgutsu@coherentsolutions.com>
2023-02-24 21:17:02 +0100

    Add spock tests, oauth2 resource server and angular frontend module

--- dependencies/security/src/main/java/org/zodiac/security/config/ResourceServerConfig.java
+++ dependencies/security/src/main/java/org/zodiac/security/config/ResourceServerConfig.java
@@ -1,21 +1,38 @@
 package org.zodiac.security.config;
 
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.zodiac.security.converter.KCRoleConverter;
 
 @Configuration
 public class ResourceServerConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf().disable()
+				.cors().and()
 				.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
 				.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
 		return http.build();
+	}
+
+	@Bean
+	public JwtAuthenticationConverter jwtAuthenticationConverter() {
+		JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KCRoleConverter());
+		return jwtAuthenticationConverter;
+	}
+
+	@Bean
+	public GrantedAuthorityDefaults grantedAuthorityDefaults() {
+		return new GrantedAuthorityDefaults(Strings.EMPTY);
 	}
 
 	@Bean
